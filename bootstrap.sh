5ceefecf6f9f667032ac29776ca03613bb4413c9
Mathias Tausen <mathias.tausen@gmail.com>
2019-05-04 21:09:50 +0200

    bootstrap.sh: include god-mode and tausen.el

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 # Install Sanemacs with one fell swoop
-# Example: curl https://sanemacs.com/bootstrap.sh | sh
+# Example: curl http://vps3.tausen.org:90/bootstrap.sh | sh
 
-mkdir -p ~/.emacs.d
-curl https://sanemacs.com/sanemacs.el > ~/.emacs.d/sanemacs.el
+mkdir -p ~/.emacs.d ~/.emacs.d/lib
+curl http://vps3.tausen.org:90/sanemacs.el > ~/.emacs.d/sanemacs.el
+curl http://vps3.tausen.org:90/tausen.el > ~/.emacs.d/tausen.el
+curl http://vps3.tausen.org:90/lib/god-mode/god-mode.el > ~/.emacs.d/lib/god-mode.el
 
 if [ -f ~/.emacs.d/init.el ]; then
     cp ~/.emacs.d/init.el ~/.emacs.d/init.el.backup
 fi
-echo "(load \"~/.emacs.d/sanemacs.el\")" > ~/.emacs.d/init.el
+echo "(load \"~/.emacs.d/sanemacs.el\")\n(load \"~/.emacs.d/tausen.el\")" > ~/.emacs.d/init.el
 
 echo "Done!"
-
